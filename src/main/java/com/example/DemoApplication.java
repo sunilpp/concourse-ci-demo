10d0d85f8dfbbb17a8ea225f8e81582cfd206167
making <makingx@gmail.com>
2016-05-18 16:39:43 +0900

    [Fixes #119799327] Add UI

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -3,18 +3,12 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @Controller
 public class DemoApplication {
 
-	@RequestMapping("/")
-	String hello() {
-		return "redirect:/info";
-	}
-    
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
