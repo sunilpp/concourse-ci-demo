90b84c819995e27d1f5d03d826092c8bf7abb1d5
making <makingx@gmail.com>
2016-05-19 03:12:37 +0900

    Change commit log message

--- ci/tasks/bump-to-next-develop-version.sh
+++ ci/tasks/bump-to-next-develop-version.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 VERSION=`cat version/number`-SNAPSHOT
-MESSAGE="Bump to Next Development Version ($VERSION)"
+MESSAGE="[Concourse CI] Bump to Next Development Version ($VERSION)"
 
 cd out
 shopt -s dotglob
 mv -f ../repo/* ./
 git remote add -f master ../repo-master
 git merge --no-edit master/master
-echo "[Concourse CI] Bump to ($VERSION)"
+echo "Bump to $VERSION"
 ./mvnw versions:set -DnewVersion=${VERSION} -DallowSnapshots -Dmaven.repo.local=../m2/rootfs/opt/m2
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
--- ci/tasks/bump-to-release-version.sh
+++ ci/tasks/bump-to-release-version.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 VERSION=`cat version/number`
-MESSAGE="Release $VERSION"
+MESSAGE="[Concourse CI] Release $VERSION"
 
 cd out
 shopt -s dotglob
 mv -f ../repo/* ./
-echo "[Concourse CI] Bump to ($VERSION)"
+echo "Bump to ($VERSION)"
 ./mvnw versions:set -DnewVersion=${VERSION} -Dmaven.repo.local=../m2/rootfs/opt/m2
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
