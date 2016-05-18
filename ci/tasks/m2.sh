97c895907708738ac98c1a8450c05c1ef7c111f3
making <makingx@gmail.com>
2016-05-18 13:35:54 +0900

    Add docker push

--- ci/tasks/m2.sh
+++ ci/tasks/m2.sh
@@ -6,6 +6,8 @@ fi
 
 cd repo
 ./mvnw clean package -Dmaven.repo.local=../m2/rootfs/opt/m2
+# download install versions plugin
+./mvnw versions:set -DnewVersion=5.0.0 -Dmaven.repo.local=../m2/rootfs/opt/m2
 cd ../m2
 tar -C rootfs -cf rootfs.tar .
 mv rootfs.tar ../to-push
