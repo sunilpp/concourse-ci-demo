99e6b7b421558e12cd699e1420ee4dacd74439b3
making <makingx@gmail.com>
2016-05-18 20:12:05 +0900

    Fix

--- ci/tasks/update-pom.sh
+++ ci/tasks/update-pom.sh
@@ -13,7 +13,7 @@ shopt -s dotglob
 mv -f ../repo/* ./
 echo "Bump to ($VERSION)"
 ./mvnw versions:set -DnewVersion=${VERSION} -DallowSnapshots -Dmaven.repo.local=../m2/rootfs/opt/m2
-git config --global user.email "{{git-email}}"
-git config --global user.name "{{git-name}}"
+git config --global user.email "${GIT_EMAIL}"
+git config --global user.name "${GIT_NAME}"
 git add pom.xml
 git commit -m "${MESSAGE}"
