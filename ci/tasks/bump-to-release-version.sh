88bf7866917c3a286d9f8cb6aee05656d70bf1fe
making <makingx@gmail.com>
2016-05-19 01:29:30 +0900

    Fix pipeline

---
+++ ci/tasks/bump-to-release-version.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+VERSION=`cat version/number`
+MESSAGE="Release $VERSION"
+
+cd out
+shopt -s dotglob
+mv -f ../repo/* ./
+echo "Bump to ($VERSION)"
+./mvnw versions:set -DnewVersion=${VERSION} -Dmaven.repo.local=../m2/rootfs/opt/m2
+git config --global user.email "${GIT_EMAIL}"
+git config --global user.name "${GIT_NAME}"
+git add pom.xml
+git commit -m "${MESSAGE}"
